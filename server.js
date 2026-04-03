0f6a1a251ad6537850ba345e2895a0b214bcf41a
Manideepsai <nmanideepsai25@gmail.com>
2026-04-03 19:14:26 +0530

    User model created with password hashing

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+require("dotenv").config();
 const app=require("./src/app");
+const connectDB=require("./src/config/db");
 
-const PORT=5000;
-
+connectDB();
+const PORT=process.env.PORT ||5000;
 app.listen(PORT,()=>{
     console.log(`Server running om port ${PORT}`);
 });
+console.log(process.env.MONGO_URI);
