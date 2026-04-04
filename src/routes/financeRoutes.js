37cb55c3de3b414b6ff5d05c7eb5587dd6c4295b
Manideepsai <nmanideepsai25@gmail.com>
2026-04-04 13:48:12 +0530

    Finance create and get APIs implemented with auth protection

--- src/routes/financeRoutes.js
+++ src/routes/financeRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
 const router = express.Router();
-
-const {createRecord} = require("../controllers/financeController");
-
+const {createRecord,getRecords} = require("../controllers/financeController");
 const protect = require("../middleware/authMiddleware");
 
+
+
 router.post("/", protect, createRecord);
+router.get("/", protect, getRecords);
 
 module.exports = router;
