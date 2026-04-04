616be64a95c6da657412bd7d4185a378f499c81d
Manideepsai <nmanideepsai25@gmail.com>
2026-04-04 14:09:58 +0530

    Get single financial record API implemented with access control

--- src/controllers/financeController.js
+++ src/controllers/financeController.js
@@ -1,5 +1,5 @@
 const FinancialRecord = require("../models/FinancialRecord");
-
+const mongoose=require("mongoose");
 const createRecord = async (req,res)=>{
     try{
         const {amount,type,category,description} = req.body;
@@ -67,8 +67,69 @@ const getRecords = async (req,res)=>{
         });
     }
 };
+const getSingleRecord = async (req,res)=>{
 
+    try{
+
+       if(!mongoose.Types.ObjectId.isValid(req.params.id)){
+
+    return res.status(400).json({
+        success:false,
+        message:"Invalid record ID"
+    });
+
+}
+
+const record = await FinancialRecord.findById(req.params.id);
+
+        if(!record || record.isDeleted){
+
+            return res.status(404).json({
+                success:false,
+                message:"Record not found"
+            });
+
+        }
+
+        // admin can view any
+        if(req.user.role === "ADMIN"){
+
+            return res.json({
+                success:true,
+                data:record
+            });
+
+        }
+
+        // user can view only own
+        if(record.userId.toString() !== req.user.userId){
+
+            return res.status(403).json({
+                success:false,
+                message:"Not authorized"
+            });
+
+        }
+
+        res.json({
+            success:true,
+            data:record
+        });
+
+    }catch(error){
+
+        console.log(error);
+
+        res.status(500).json({
+            success:false,
+            message:error.message
+        });
+
+    }
+
+};
 module.exports = {
     createRecord,
-    getRecords
+    getRecords,
+    getSingleRecord
 };
--- src/routes/financeRoutes.js
+++ src/routes/financeRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 
 const router = express.Router();
-const {createRecord,getRecords} = require("../controllers/financeController");
+const {createRecord,getRecords,getSingleRecord} = require("../controllers/financeController");
 const protect = require("../middleware/authMiddleware");
 
 
 
 router.post("/", protect, createRecord);
 router.get("/", protect, getRecords);
+router.get("/:id", protect, getSingleRecord);
 
 module.exports = router;
