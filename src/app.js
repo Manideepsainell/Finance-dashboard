f672931d73467e427fcbd8fb30b7c7a81daece87
Manideepsai <nmanideepsai25@gmail.com>
2026-04-04 13:22:02 +0530

    Auth middleware implemented

--- src/app.js
+++ src/app.js
@@ -2,12 +2,22 @@ const express=require("express");
 
 const app=express();
 const authRoutes = require("./routes/authRoutes");
+const protect = require("./middleware/authMiddleware");
 //middleware
 app.use(express.json());
 
 //test
 app.get("/test",(req,res)=>{
     res.send("API working");
+});
+app.get("/protected", protect, (req,res)=>{
+
+    res.json({
+        success:true,
+        message:"Protected route works",
+        user:req.user
+    });
+
 });
 app.use("/auth", authRoutes);
 
